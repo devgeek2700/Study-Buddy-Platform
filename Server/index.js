4684100a0b0ad92b2a54251d6e98d5013af91943
Neha Singh <128679725+devgeek2700@users.noreply.github.com>
2024-04-26 19:53:35 +0530

    Add files via upload

---
+++ Server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "Private_Key_ChatEngine" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.status);
+  }
+
+  return res.json({ username: username, secret: "chat1234" });
+});
+
+app.listen(3001);
